7472656f1ac10c8d97d23a95487f9a3059a57d46
isadoraspirov <dora.isinha2@gmail.com>
2026-06-30 20:51:17 +0100

    update settings and templates, add initial migrations and requirements

--- nutrition/models.py
+++ nutrition/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class NutritionPlan(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    image = models.ImageField(
+        upload_to="nutrition/",
+        blank=True,
+        null=True
+    )
+
+    duration_weeks = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class WorkoutPlan(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    level = models.CharField(
+        max_length=50
+    )
+
+    image = models.ImageField(
+        upload_to="workouts/",
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
