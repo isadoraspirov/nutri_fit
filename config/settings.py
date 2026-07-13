3ab51ff7d4d14d3dea74d9af1d64712a5fc6aae5
isadoraspirov <dora.isinha2@gmail.com>
2026-07-13 20:08:51 +0100

    Temporarily enable debug for Heroku troubleshooting

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
